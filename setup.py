86590f2a31174cee1189735f04eb80e747691df0
Renou Benteau <abenteau@gmail.com>
2021-12-20 11:24:27 +0100

    restructured to allow easy installation of domain_db.models

--- setup.py
+++ setup.py
@@ -5,12 +5,21 @@ setup(
   version='0.0.1',
   author='Renou Benteau',
   author_email='rebe01@dfki.de',
-  packages=['sqlalchemy_tools'],
-  description='Generate SQLAclhemy database url, connect to session',
+  packages=['sqlalchemy_tools', 'domain_db'],
+  description='Generate SQLAclhemy database url, connect to session. Domain DB with defined models',
   install_requires=[
-      "python-dotenv==0.19.2",
-      "psycopg2==2.9.2",
-      "SQLAlchemy==1.4.28"
-
-  ]
+    "alembic==1.7.5",
+    "certifi==2021.10.8",
+    "greenlet==1.1.2",
+    "Mako==1.1.6",
+    "MarkupSafe==2.0.1",
+    "numpy==1.21.4",
+    "pandas==1.3.5",
+    "psycopg2==2.9.2",
+    "python-dateutil==2.8.2",
+    "python-dotenv==0.19.2",
+    "pytz==2021.3",
+    "six==1.16.0",
+    "SQLAlchemy==1.4.28"
+    ]
 )

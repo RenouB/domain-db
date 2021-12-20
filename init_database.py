062681a169c7c811642fefde7745a989c5d9817f
Renou Benteau <abenteau@gmail.com>
2021-12-20 10:26:46 +0100

    update sqlalchemy_tools to match latest python-dotenv version

--- init_database.py
+++ init_database.py
@@ -5,16 +5,19 @@ import pandas as pd
 session = get_sqlalchemy_session()
 
 if __name__ == "__main__":
-    # gemuese_df = pd.read_csv("./data/gemuese.csv")
-    # gemuese_list = gemuese_df.to_dict(orient="records")
+    if not len(session.query(Gemuese).all()):
+        print("adding some stuff to database")
+        gemuese_df = pd.read_csv("./data/gemuese.csv")
+        gemuese_list = gemuese_df.to_dict(orient="records")
 
-    # gemuese_to_add = []
-    # for gemuese in gemuese_list:
-    #     gemuese_to_add.append(Gemuese(**gemuese))
+        gemuese_to_add = []
+        for gemuese in gemuese_list:
+            gemuese_to_add.append(Gemuese(**gemuese))
+            
+        session.add_all(gemuese_to_add)
+        session.commit()
+        session.close()
         
-    # session.add_all(gemuese_to_add)
-    # session.commit()
-    # session.close()
     print(session.query(Gemuese).all())
 
 
